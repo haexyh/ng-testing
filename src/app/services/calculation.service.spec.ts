cb4a83f01e0bca0b76abf3e440c8da51eb1b990a
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-27 09:45:15 +0200

    added v1

---
+++ src/app/services/calculation.service.spec.ts
@@ -0,0 +1,33 @@
+import { TestBed } from '@angular/core/testing';
+
+import { CalculationService } from './calculation.service';
+
+describe('CalculationService', () => {
+  let service: CalculationService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(CalculationService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should add two numbers', () => {
+    fail()
+  });
+
+  it('should be correct', () => {
+    const result = service.divide(1,1)
+    expect(result).toBe(1);
+  });
+
+  it('should be infinity ', () => {
+    const result = service.divide(1,2)
+    expect(result)
+      .withContext('Second number isn`t 0')
+      .toBe(Infinity);
+  });
+
+});
---
+++ src/app/services/calculation.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CalculationService {
+
+  add(a: number, b: number)
+  {
+    return a+b;
+  }
+  divide(a: number, b: number)
+  {
+    return a /b;
+  }
+
+}
