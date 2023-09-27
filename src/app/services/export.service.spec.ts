ec5745f897438940980163f18afda28679025154
Pascal Klaiss <pascal.klaiss@smino.com>
2023-09-27 20:21:08 +0200

    clean up

--- src/app/services/export.service.spec.ts
+++ src/app/services/export.service.spec.ts
@@ -4,21 +4,17 @@ import {ExportService} from './export.service';
 import {AsyncService} from "./async.service";
 import createSpyObj = jasmine.createSpyObj;
 import {HttpClientTestingModule} from "@angular/common/http/testing";
-import Jasmine = jasmine.Jasmine;
-import Spy = jasmine.Spy;
 
-fdescribe('ExportService', () => {
+describe('ExportService', () => {
   let service: ExportService, asyncService: any;
 
   beforeEach(() => {
-    const mock  = createSpyObj('AsyncService', ['isConnected']);
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [{provide: AsyncService, use: mock}]
+      providers: [{provide: AsyncService, use: createSpyObj('AsyncService', ['isConnected'])}]
     });
     service = TestBed.inject(ExportService);
     asyncService = TestBed.inject(AsyncService)
-
   });
 
   it('should be created', () => {
@@ -28,13 +24,13 @@ fdescribe('ExportService', () => {
   });
 
   it('can`t export', () => {
-    spyOn(asyncService,'isConnected'); // mock it
+    spyOn(asyncService, 'isConnected'); // mock it
     const result = service.canExport();
     expect(result).toBeFalsy();
   });
 
   it('can  export', () => {
-    spyOn(asyncService,'isConnected').and.returnValue(true)
+    spyOn(asyncService, 'isConnected').and.returnValue(true)
     const result = service.canExport();
     expect(result).toBe(true);
   });
